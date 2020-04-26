c0ceccdee29753f598bf6ef2090dd55dd0daf195
Kent McKinney <kent.p.mckinney@gmail.com>
2020-04-26 11:28:07 -0700

    add layout grid

--- src/component/App.js
+++ src/component/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <div class="parent">
+        <div class="div1">Merch Site</div>
+        <div class="div2">Cart</div>
+        <div class="div3">Item 1</div>
+        <div class="div4">Item 1</div>
+      </div>
     </div>
   );
 }
