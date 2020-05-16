c3150f739061d0b592f6d8ab4fe67d525c43f87d
wrunion <winterrunion@gmail.com>
2020-05-15 19:50:25 -0700

    Write test for deleting an item.

--- src/__tests__/reducer.test.js
+++ src/__tests__/reducer.test.js
@@ -5,8 +5,6 @@ describe('reducer', () => {
     expect(reducer([],{type: null})).toEqual([]);
   });
 
-
-
   let action;
   const itemData = [
     {
@@ -28,7 +26,6 @@ describe('reducer', () => {
   test('Should add new item data to state', () => {
     expect(reducer([], {type: 'ADD_ITEM', data: {...itemData[0]} })).toEqual([{...itemData[0]}]);
   });
-
   
   test('Should replace item data if id already exists', () => {
     expect(reducer(itemData, {type: 'EDIT_ITEM', data: {name: 'Green shirt', description: "Green shirt description", available: 10, price: 10, id: 1}})).toEqual(
@@ -51,17 +48,35 @@ describe('reducer', () => {
     )
   });
 
+  const itemDataBeforeDelete = [
+    {
+      name: 'Blue shirt',
+      description: 'Blue shirt description',
+      available: 10,
+      price: 10,
+      id: 1
+    },
+    {
+      name: 'Red shirt',
+      description: 'Red shirt description',
+      available: 10,
+      price: 10,
+      id: 2
+    }
+  ]
 
+  const itemDataAfterDelete = [
+    {
+      name: 'Red shirt',
+      description: 'Red shirt description',
+      available: 10,
+      price: 10,
+      id: 2
+    }
+  ]
+
+  test('Should delete an item based on id', () => {
+    expect(reducer(itemDataBeforeDelete, {type: 'DELETE_ITEM', data: 1})).toEqual(itemDataAfterDelete);
+  });
+  
 });
-
-//name, description, available, price, key
-
-  // [
-  //   {
-  //     name: 'Blue shirt',
-  //     description: 'Blue shirt description',
-  //     available: 10,
-  //     price: 10,
-  //     id: 1
-  //   }
-  // ]
